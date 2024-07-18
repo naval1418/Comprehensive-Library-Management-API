97a50e66fda3bb2297f9bb3712a4fe439261b0ab
naval1418 <navalsaik@gmail.com>
2024-07-18 12:15:21 -0600

    add initial files

---
+++ database.js
@@ -0,0 +1,121 @@
+const authors = [
+    { id: 1, name: "J.K. Rowling", booksWritten: ["978-0747532699", "978-0439064873"] },
+    { id: 2, name: "George R.R. Martin", booksWritten: ["978-0553103540", "978-0553579901"] },
+    { id: 3, name: "J.R.R. Tolkien", booksWritten: ["978-0547928227", "978-0618640157"] },
+    { id: 4, name: "Isaac Asimov", booksWritten: ["978-0553293357", "978-0586010808"] },
+    { id: 5, name: "Arthur C. Clarke", booksWritten: ["978-0451457998", "978-0345325815"] }
+  ];
+  
+  const books = [
+    {
+      ISBN: "978-0747532699",
+      title: "Harry Potter and the Philosopher's Stone",
+      pubDate: "1997-06-26",
+      language: "English",
+      numOfPages: 223,
+      author: 1,
+      publication: 1,
+      category: "Fantasy"
+    },
+    {
+      ISBN: "978-0439064873",
+      title: "Harry Potter and the Chamber of Secrets",
+      pubDate: "1998-07-02",
+      language: "English",
+      numOfPages: 251,
+      author: 1,
+      publication: 1,
+      category: "Fantasy"
+    },
+    {
+      ISBN: "978-0553103540",
+      title: "A Game of Thrones",
+      pubDate: "1996-08-06",
+      language: "English",
+      numOfPages: 694,
+      author: 2,
+      publication: 2,
+      category: "Fantasy"
+    },
+    {
+      ISBN: "978-0553579901",
+      title: "A Clash of Kings",
+      pubDate: "1998-11-16",
+      language: "English",
+      numOfPages: 768,
+      author: 2,
+      publication: 2,
+      category: "Fantasy"
+    },
+    {
+      ISBN: "978-0547928227",
+      title: "The Hobbit",
+      pubDate: "1937-09-21",
+      language: "English",
+      numOfPages: 310,
+      author: 3,
+      publication: 3,
+      category: "Fantasy"
+    },
+    {
+      ISBN: "978-0618640157",
+      title: "The Lord of the Rings",
+      pubDate: "1954-07-29",
+      language: "English",
+      numOfPages: 1216,
+      author: 3,
+      publication: 3,
+      category: "drama"
+    },
+    {
+      ISBN: "978-0553293357",
+      title: "Foundation",
+      pubDate: "1951-05-01",
+      language: "English",
+      numOfPages: 255,
+      author: 4,
+      publication: 4,
+      category: "time"
+    },
+    {
+      ISBN: "978-0586010808",
+      title: "I, Robot",
+      pubDate: "1950-12-02",
+      language: "English",
+      numOfPages: 253,
+      author: 4,
+      publication: 4,
+      category: "futuristic"
+    },
+    {
+      ISBN: "978-0451457998",
+      title: "2001: A Space Odyssey",
+      pubDate: "1968-07-01",
+      language: "English",
+      numOfPages: 297,
+      author: 5,
+      publication: 5,
+      category: "Science_Fiction"
+    },
+    {
+      ISBN: "978-0345325815",
+      title: "Rendezvous with Rama",
+      pubDate: "1973-08-10",
+      language: "English",
+      numOfPages: 256,
+      author: 5,
+      publication: 5,
+      category: "action"
+    }
+  ];
+  
+  const publications = [
+    { id: 1, name: "Bloomsbury", books: ["978-0747532699", "978-0439064873"] },
+    { id: 2, name: "Bantam Books", books: ["978-0553103540", "978-0553579901"] },
+    { id: 3, name: "George Allen & Unwin", books: ["978-0547928227", "978-0618640157"] },
+    { id: 4, name: "Gnome Press", books: ["978-0553293357", "978-0586010808"] },
+    { id: 5, name: "Roc", books: ["978-0451457998", "978-0345325815"] }
+  ];
+
+
+  module.exports= {authors,books,publications} ;
---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const app = express();
+const database = require('./database');
+const port = 4000;
+
+
+
+app.get("/", (req, res) => {
+     return res.json(database.books);
+});
+
+/*
+route : /is
+description : get a specific book 
+access : public 
+parameter : isbn
+methods : get 
+*/
+app.get("/is/:isbn", (req,res) => {
+
+         const infobook = database.books.filter(
+            (book) => book.ISBN == req.params.isbn 
+        );
+
+        if(infobook.length === 0 ){
+               res.json({error : `no book found with the ISBN no ${req.params.isbn}`}) ;
+        }    
+        res.send(infobook) ;
+})
+
+
+
+/*
+route : /c
+description : get a specific book 
+access : public 
+parameter : category
+methods : get 
+*/
+app.get("/c/:Category", (req,res) => {
+
+    const getSpecificBook = database.books.filter(
+       (book) => book.category == req.params.Category 
+   );
+
+   if(getSpecificBook.length === 0 ){
+          res.json({error : `no book found with the Category ${req.params.Category}`}) ;
+   }    
+   res.send(getSpecificBook) ;
+})
+
+/*
+route : /author
+description : get all the authors
+access : public 
+methods : get 
+*/
+
+app.get("/authors", (req,res) => {
+    res.json({authors : database.authors});
+})
+
+/*
+route : /author
+description : get specific author base on id
+access : public 
+parameter : id
+methods : get 
+*/
+
+app.get("/authors/:ID", (req,res) => {
+    const authors_id = database.authors.filter(aut => aut.id === parseInt(req.params.ID)) ;
+
+    res.json({author : authors_id })
+})
+
+/*
+route : /publications
+description : get all the publications
+access : public 
+parameter : none
+methods : get 
+*/
+
+
+app.get("/publications" , (req,res) => {
+    res.json({publications : database.publications})
+})
+
+/*
+route : /publications
+description : get the specific publication based on book
+access : public 
+parameter : specificbook
+methods : get 
+*/
+
+app.get("/publications/:specificbook" , (req,res) => {
+    const pub = database.publications.filter ( Book => Book.books.includes(req.params.specificbook))
+       console.log(pub.length) ;
+    res.json({publicaion : pub})
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
